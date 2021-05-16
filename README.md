2473e24d42d12b4ce4b6a2a03bf233fc57a529e7
Murat Bayraktar <muratbayrktr@users.noreply.github.com>
2021-05-17 01:37:16 +0300

    algorithm implemented, time complexity will be reviewed

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # fast-determinant-calculation  
 Trying a new algorithm to calculate determinant in a fast way.  
 
-It uses next_permutation algorithm the compute the next permutation in O(n).  
+It uses next_permutation algorithm the compute the next permutation in O(n). I added swap count   
+in order to store the swaps so that we can infer whether the permutation is even or odd.    
+With this way, I could implement the big formula for the determinant calculation.  
+
 
 By doing so,  
 
 detA = SUM[ +- a1x*a2y*a3z ... anbeta ],  
 
-x , y , z ... beta  are the permutations of the 1,2 ... n  (which next permutation calculates at every iteration).   
+x , y , z ... beta  are the permutations of the 1,2 ... n  (which next permutation calculates at every iteration).  
+
+```
+$ ./determinantcalc < input.txt 
+-83550
+Program took 0.000227 seconds to execute
+```
+
--- determinantcalc.c
+++ determinantcalc.c
@@ -2,9 +2,11 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <math.h>
+#include <time.h>
+#define MAXLEN 7
 
-#define MAXLEN 4
-
+/* this static variable is for calculating whether the permutation is even or odd */
+static int total_swaps = 0;
 
 /* 
  * Computes the next lexicographical permutation of the specified array of integers in place,
@@ -30,14 +32,15 @@ bool next_permutation(int array[], size_t length) {
 	temp = array[i - 1];
 	array[i - 1] = array[j];
 	array[j] = temp;
-	
+	total_swaps++;
 
 	j = length - 1;
 	while (i < j) {
 		temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
-		i++;
+		total_swaps++;
+        i++;
 		j--;
 	}
 	return true;
@@ -55,27 +58,34 @@ void input_matrix(int matrix[MAXLEN][MAXLEN])
 
 int main(void)
 {
-    int i,j, k = 0,matrix[MAXLEN][MAXLEN] = {0}, array_of_perms[MAXLEN] = {0};
+    int i,num, matrix[MAXLEN][MAXLEN] = {0}, array_of_perms[MAXLEN] = {0};
     bool availability = true;
-    double sum = 0.0;
+    double sum = 0.0,time_taken;
+    clock_t t;
+    
     input_matrix(matrix);
-    for (i = 1; i <= MAXLEN; i++) array_of_perms[i] = i;
 
-    do
-    {
-        for (i = 1; i <= MAXLEN; i++)
+    for (i = 0; i < MAXLEN; i++) array_of_perms[i] = i+1;
+
+    
+    t = clock();
+    while (availability)
+    {   
+        num=1;
+        /* printf("sign: %.0f",sign); */
+        for (i = 0; i < MAXLEN; i++) 
         {
-            for ( j = 0; j < MAXLEN; j++)
-            {
-                printf(" %d,%d ",i,array_of_perms[j]);
-            }
-            printf("\n");
+            num *= matrix[i][array_of_perms[i]-1];
+            /*printf(" %d,%d -> %d \t\t",i,array_of_perms[i]-1,num);*/
+            if (num == 0) break;
         }
+        /* printf("Product: %.0f\n",sign*num); */
+        sum += pow(-1,total_swaps)*num;
         availability = next_permutation(array_of_perms, MAXLEN);
-    } while (availability);
-    printf("%f\n",sum);
-
-
-
+    }
+    t = clock() - t;
+    time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("%.0f\n",sum);
+    printf("Program took %f seconds to execute \n", time_taken);
     return 0;
 }
